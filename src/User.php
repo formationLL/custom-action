f3dbe70e7cb3cf919f30beefed9ab1810ffea5dc
Julian Layen <julian.layen@gmail.com>
2023-03-30 11:11:36 +0200

    first commit

---
+++ src/User.php
@@ -0,0 +1,8 @@
+<?php
+namespace App;
+
+class User {
+	public function getName($name) {
+		return "Hello " . $name;
+	}
+}
---
+++ test/GreeterTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Test;
+
+use App\User;
+
+class UserTest extends \PHPUnit\Framework\TestCase {
+	public function testUserHello() {
+		$user = new User();
+		self::assertStringContainsString(
+			"Hello",
+			$user->getName()
+		);
+	}
+}
